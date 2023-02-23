ce6674c51389bfe2b40896811d8ca460a15276a3
Lucas Smaniotto <lucas91755@gmail.com>
2023-02-23 09:40:03 -0300

    refactor: mudando lógica de funcionamento do hook ScrollToTop

--- src/hooks/ScrollToTop.jsx
+++ src/hooks/ScrollToTop.jsx
@@ -3,10 +3,15 @@ import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
+  const title = document.querySelector('#root > main > article > h2');
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    if (title) {
+      title.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }, [pathname, title]);
 
   return null;
 }

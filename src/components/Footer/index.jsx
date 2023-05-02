897298f45c846c000800c19f01004d447f35fe59
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-05-02 10:59:40 -0300

    style: ajustando links do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <p>Deselvolvido por <a href="https://github.com/lucassmaniotto" target="_blank" rel="noreferrer">mim mesmo</a> 😃</p>
         <div className={styles.socialLinks}>
             <a href="https://github.com/lucassmaniotto"><AiFillGithub size={25}/></a>
-            <a href="https://www.instagram.com/corvus_tenebrae"><AiOutlineInstagram size={25}/></a>
+            <a href="https://www.instagram.com/licaaaaaas"><AiOutlineInstagram size={25}/></a>
             <a href="https://twitter.com/alpiste_punk"><AiOutlineTwitter size={25}/></a>
         </div>
     </footer>

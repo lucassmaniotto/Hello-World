6ee9e1eb94afa61c90638a2c772d476c7c28dc26
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-05-04 10:49:48 -0300

    fix: atualizando links de certificados
    Os certificados antes só poderiam ser acessados se o usuário estava logado na alura

--- src/components/Grid/CertificatesGrid/index.jsx
+++ src/components/Grid/CertificatesGrid/index.jsx
@@ -15,35 +15,35 @@ export default function TecnologiesGrid() {
         title={"Formação React com JavaScript"}
         paragraph={"Alura"}
         date={"31/01/2023"}
-        link={"https://cursos.alura.com.br/user/lucassmaniotto/degree-react-javascript-450960/certificate"}
+        link={"https://cursos.alura.com.br/degree/certificate/e3daef71-12e9-4253-81cc-0cc4c6ecbfee"}
         icon={"https://www.alura.com.br/assets/api/formacoes/categorias/front-end.svg"}
       />
       <CardAncor
         title={"Formação JavaScript para Front-end"}
         paragraph={"Alura"}
         date={"02/01/2023"}
-        link={"https://cursos.alura.com.br/user/lucassmaniotto/degree-javascript-front-end-374365/certificate"}
+        link={"https://cursos.alura.com.br/degree/certificate/e073304a-fc7f-4c88-97fd-12864f221ef7"}
         icon={"https://www.alura.com.br/assets/api/formacoes/categorias/front-end.svg"}
       />
       <CardAncor
         title={"Formação HTML e CSS"}
         paragraph={"Alura"}
         date={"29/11/2022"}
-        link={"https://cursos.alura.com.br/user/lucassmaniotto/degree-html-e-css-v232036-232036/certificate"}
+        link={"https://cursos.alura.com.br/degree/certificate/3b13b8ca-42a3-4491-b003-4ab0bacedcbe"}
         icon={"https://www.alura.com.br/assets/api/formacoes/categorias/front-end.svg"}
       />
       <CardAncor
         title={"Formação QA: processos e automação de testes"}
         paragraph={"Alura"}
         date={"04/08/2022"}
-        link={"https://cursos.alura.com.br/user/lucassmaniotto/degree-carreira-tester-qa-281969/certificate"}
+        link={"https://cursos.alura.com.br/degree/certificate/465bbfb9-30e8-4b65-907d-326218ebf87f"}
         icon={"https://www.alura.com.br/assets/api/formacoes/categorias/programacao.svg"}
       />
       <CardAncor
         title={"Formação JavaScript para Back-end"}
         paragraph={"Alura"}
         date={"31/10/2022"}
-        link={"https://cursos.alura.com.br/user/lucassmaniotto/degree-js-backend-210587/certificate"}
+        link={"https://cursos.alura.com.br/degree/certificate/c355e96a-817b-40d2-9150-3cbda076bd64"}
         icon={"https://www.alura.com.br/assets/api/formacoes/categorias/programacao.svg"}
       />
       <CardAncor

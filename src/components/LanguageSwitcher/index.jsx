f7abd708a6751baed410eb37d2dc8b5b2be2f2cd
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-11-01 16:32:09 -0300

    fix: traduzindo restante dos filtros

--- src/components/LanguageSwitcher/index.jsx
+++ src/components/LanguageSwitcher/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation, useNavigate } from 'react-router-dom';
 
